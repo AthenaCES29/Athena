7db3f2f2506a168d5fddbef6467d077bd15dbfa9
ftuyama <ftuyama@gmail.com>
2016-06-05 21:46:59 -0300

    Heroku 5

--- Athena/settings.py
+++ Athena/settings.py
@@ -127,8 +127,8 @@ MEDIA_URL = '/arquivos/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
