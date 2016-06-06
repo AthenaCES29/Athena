cf17636763524e1d9d0d1438f76318b1e6bb8a24
ftuyama <ftuyama@gmail.com>
2016-06-05 21:37:44 -0300

    Heroku 3

--- Athena/settings.py
+++ Athena/settings.py
@@ -133,5 +133,18 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # Session expire
 # SESSION_COOKIE_AGE = 30
