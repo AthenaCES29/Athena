8c098ba26c69bfa3ba76316228f1214f55db4e1f
ftuyama <ftuyama@gmail.com>
2016-06-11 19:45:20 -0300

    Heroku 12

--- Athena/wsgi.py
+++ Athena/wsgi.py
@@ -7,13 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-import django.core.handlers.wsgi
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Athena.settings")
-
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
