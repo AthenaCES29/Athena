7343453190f25c301b1fad2600063fccc74215e8
ftuyama <ftuyama@gmail.com>
2016-06-11 19:54:12 -0300

    Heroku 13

--- Athena/wsgi.py
+++ Athena/wsgi.py
@@ -7,13 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
-import django.core.handlers.wsgi
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Athena.settings")
-
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
